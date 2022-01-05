1de417682f4bc95b5ce99cbed584fcf8ca0a83ac
Anusha Reddy <anushareddygoguri@gmail.com>
2022-01-05 12:23:48 +0000

    React-Projects

---
+++ dbs-26th/backend/sample.js
@@ -0,0 +1,12 @@
+console.log("server side running js")
+let a=30
+let b=45
+    const result=a+b
+    console.log(result + " is the sum of two numbers")
+
+const movie= {
+    name:"Life is Beautiful",
+    desc:"good",
+    director:"abcdefgh"
+}
+console.log(movie)
